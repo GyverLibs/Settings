a64ced6e9afcd60de72afcfefca2b8e5da14d758
AlexGyver <alexgyvershow@ya.ru>
2024-07-10 18:17:16 +0300

    Create wificonnect.ino

---
+++ examples/wificonnect/wificonnect.ino
@@ -0,0 +1,76 @@
+// простой пример настройки подключения к WiFi
+// ESP работает в режиме AP_STA, чтобы всегда был доступ к настройкам через точку доступа
+// для упрощения логики работы подключение делается при старте
+// Сохранение настроек wifi перезагружает чип
+
+#include <Arduino.h>
+#include <GyverDBFile.h>
+#include <LittleFS.h>
+GyverDBFile db(&LittleFS, "settings.db");
+
+#include <SettingsESP.h>
+SettingsESP sett("WiFi config", &db);
+
+DB_KEYS(
+    kk,
+    DB_KEY(wifi_ssid),
+    DB_KEY(wifi_pass),
+    DB_KEY(apply));
+
+void build(sets::Builder& b) {
+    {
+        sets::Group g(b, "WiFi");
+        b.Input(kk::wifi_ssid, "SSID");
+        b.Pass(kk::wifi_pass, "Password");
+    }
+    {
+        sets::Buttons bt(b);
+        if (b.Button(kk::apply, "Save & Restart")) {
+            db.update();  // сохраняем БД не дожидаясь таймаута
+            ESP.reset();
+        }
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+
+    // режим AP_STA. Вызываем перед sett.begin(), чтобы
+    // settings знал о текущем режиме wifi
+    WiFi.mode(WIFI_AP_STA);
+
+    sett.begin();
+    sett.onBuild(build);
+
+    // базу данных запускаем до подключения к точке
+    LittleFS.begin();
+    db.begin();
+    db.init(kk::wifi_ssid, "");
+    db.init(kk::wifi_pass, "");
+
+    // ======= AP =======
+    WiFi.softAP("AP ESP");
+    Serial.print("AP IP: ");
+    Serial.println(WiFi.softAPIP());
+
+    // ======= STA =======
+    // если логин задан - подключаемся
+    if (db[kk::wifi_ssid].length()) {
+        WiFi.begin(db[kk::wifi_ssid], db[kk::wifi_pass]);
+        Serial.print("Connect STA");
+        int tries = 20;
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print('.');
+            if (!--tries) break;
+        }
+        Serial.println();
+        Serial.print("IP: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+
+void loop() {
+    sett.tick();
+}
