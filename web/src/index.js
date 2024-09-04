6c46ff22fb12d6c517763ff9527105d90e878f1e
AlexGyver <alexgyvershow@ya.ru>
2024-09-04 23:46:11 +0300

    upd

--- web/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css'
-import Settings from './script/settings';
-
-let settings;
-
-window.onload = () => {
-    settings = new Settings();
-}
--- web/src/script/widgets/pass.js
+++
@@ -1,8 +0,0 @@
-import InputWidget from "./input";
-
-export default class PassWidget extends InputWidget {
-    constructor(data) {
-        super(data);
-        this.$out.style.filter = 'blur(4px)';
-    }
-}
