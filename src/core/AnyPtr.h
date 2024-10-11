86646410be5e8e938a016bd278dfab9c85b5e8bd
AlexGyver <alexgyvershow@ya.ru>
2024-10-11 10:13:40 +0300

    upf

--- src/core/AnyPtr.h
+++ src/core/AnyPtr.h
@@ -7,6 +7,8 @@ class AnyPtr {
     enum class Type {
         None,
         Text,
+        ConstChar,
+        ConstFstr,
         Char,
         String,
         Int8,
@@ -19,6 +21,8 @@ class AnyPtr {
    public:
     AnyPtr() {}
     AnyPtr(std::nullptr_t p) {}
+    AnyPtr(const char* p) : p((void*)p), type(Type::ConstChar) {}
+    AnyPtr(const __FlashStringHelper* p) : p((void*)p), type(Type::ConstFstr) {}
     AnyPtr(char* p, size_t len) : p(p), len(len), type(Type::Char) {}
     AnyPtr(String* p) : p(p), type(Type::String) {}
     AnyPtr(Text* p) : p(p), type(p->valid() ? Type::Text : Type::None) {}
@@ -40,6 +44,9 @@ class AnyPtr {
 
     void write(sets::Packet* pkt) {
         switch (type) {
+            case Type::ConstChar: pkt->addText((const char*)p); break;
+            case Type::ConstFstr: pkt->addText((const __FlashStringHelper*)p); break;
+
             case Type::Text: pkt->addText(*((Text*)p)); break;
             case Type::String: pkt->addText(*((String*)p)); break;
             case Type::Char: pkt->addText((char*)p); break;
